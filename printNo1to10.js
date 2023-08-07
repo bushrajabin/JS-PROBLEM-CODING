d031d95489c27f6e984348540a69c3bf23f30709
Bushra jabin <bushrabintjabin@gmail.com>
2023-08-07 19:40:22 +0530

    1 to 10 prnt function created

---
+++ printNo1to10.js
@@ -0,0 +1,11 @@
+function printNumber(lengthofArray) {
+    var nums = [];
+    for (var i = 1; i <= lengthofArray; i++) {
+        nums.push(i)
+    }
+    return nums
+}
+const res = printNumber(10);
+console.log(res)
+
+
