ec64ea5def7292af7da88e4e5ea64c571794dbff
Bushra jabin <bushrabintjabin@gmail.com>
2023-08-07 19:24:29 +0530

    sum of array function created

---
+++ sumOfAnArray.js
@@ -0,0 +1,14 @@
+
+function SumOfArray() {
+    var arr = [10,22,322,452,766];
+    var sum = 0;
+    for (i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+let res = SumOfArray();
+console.log(res)
+
+
