bbd94768c7c6c1eb29c4ac7991457d0b07aa5ae7
Bushra jabin <bushrabintjabin@gmail.com>
2023-08-08 17:04:16 +0530

    addSuborMult function created

---
+++ add_Sub_mult.js
@@ -0,0 +1,28 @@
+function addSubMultiplyOfNumbers(a, b) {
+
+    for (let i = 1; i <= 1; i++) {
+        let add = a + b;
+        console.log(add)
+        for (let j = 1; j <= 1; j++) {
+            let sub = a - b;
+            console.log(sub);
+        }
+        for(let k=1;k<=1;k++){
+            let sub=a*b;
+            console.log(sub);
+        }
+        for(let l=1;l<=1;l++){
+            let mul=a*b;
+            console.log(mul);
+        }
+        for(let m=1;m<=1;m++){
+            let division=a*b;
+            console.log(division);
+        }
+    }
+
+
+}
+
+let result = addSubMultiplyOfNumbers(2, 4);
+console.log(result);
