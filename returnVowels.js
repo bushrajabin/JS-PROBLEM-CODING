6d8ab237cdfeed4c4fe401f8d963a22cfdb8bd9a
Bushra jabin <bushrabintjabin@gmail.com>
2023-08-13 18:05:56 +0530

    This function return vowels on a string

---
+++ returnVowels.js
@@ -0,0 +1,15 @@
+let vowels = ["a", "e", "i", "o", "u"];
+function ReturnVowels(str) {
+    let storeValue=[];
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.indexOf(str[i]) !== -1) {
+            storeValue.push(i);
+        }
+    }
+    return storeValue;
+}
+
+
+
+let result = ReturnVowels("bushra");
+console.log(result);
