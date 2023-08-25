761215556cce921bace461f78bc1a15eaef94262
Bushra jabin <bushrabintjabin@gmail.com>
2023-08-25 20:00:09 +0530

    switch case

---
+++ switchCase.js
@@ -0,0 +1,15 @@
+var Number = 5;
+
+switch (Number) {
+    case 5:
+        console.log("this is 5!!!");
+        break;
+    case 30:
+        console.log("People say number 30 gives bad luck");
+        break;
+    case 99:
+        console.log("I like number 99!!");
+        break;
+    default:
+        console.log("Meh, just a boring number");
+}
